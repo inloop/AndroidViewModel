c00ed9e8ea6bf2b7c314217003942cd4d8795019
Juraj Novák <jnovak@inloop.eu>
2017-02-18 09:40:43 +0100

    Add getViewOptional method - NonNull getView variant

---
+++ library/src/main/java/eu/inloop/viewmodel/ProxyViewHelper.java
@@ -0,0 +1,53 @@
+package eu.inloop.viewmodel;
+
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Proxy;
+import java.lang.reflect.Type;
+
+class ProxyViewHelper {
+
+    private static final class ProxyDummyClass {
+    }
+
+    private static final ProxyDummyClass sDummyClass = new ProxyDummyClass();
+    private static final Class[] sInterfaces = new Class[1];
+
+    private ProxyViewHelper() {
+    }
+
+    @SuppressWarnings("unchecked")
+    @NonNull
+    static <T> T init(@NonNull Class<?> in) {
+        sInterfaces[0] = in;
+        return (T) Proxy.newProxyInstance(sDummyClass.getClass().getClassLoader(), sInterfaces, sInvocationHandler);
+    }
+
+    @Nullable
+    static Class<?> getGenericType(@NonNull Class<?> in, @NonNull Class<?> whichExtends) {
+        final Type genericSuperclass = in.getGenericSuperclass();
+        if (genericSuperclass instanceof ParameterizedType) {
+            final Type[] typeArgs = ((ParameterizedType) genericSuperclass).getActualTypeArguments();
+            for (Type arg : typeArgs) {
+                final Class<?> argClass = (Class<?>) arg;
+                if (whichExtends.isAssignableFrom(argClass)) {
+                    return argClass;
+                }
+            }
+        }
+        return null;
+    }
+
+    private static final InvocationHandler sInvocationHandler = new InvocationHandler() {
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            return null;
+        }
+    };
+
+}
